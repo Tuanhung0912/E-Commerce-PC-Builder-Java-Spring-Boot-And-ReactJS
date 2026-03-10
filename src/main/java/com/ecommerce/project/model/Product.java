3099d85990c03abb49b1e91b82192de4309cbb48
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-03-10 21:39:36 +0700

    Add ProductDTO and ProductResponse

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
     private String productName;
     private String description;
