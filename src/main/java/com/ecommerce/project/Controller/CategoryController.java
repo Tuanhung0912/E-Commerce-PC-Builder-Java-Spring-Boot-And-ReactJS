3e89d7b9bef5790d4f4a44241fb3c1df7379056a
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-03-10 15:18:40 +0700

    Use ModelMaper and add CategoryDTO

--- src/main/java/com/ecommerce/project/Controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/Controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.Controller;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.service.CategoryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,9 +27,9 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     //@RequestMapping(value = "/public/categories", method = RequestMethod.GET)
-    public ResponseEntity<List<Category>> getAllCategories(){
-        List<Category> categories = categoryService.getAllCategories();
-        return new ResponseEntity<>(categories, HttpStatus.OK);
+    public ResponseEntity<CategoryResponse> getAllCategories(){
+        CategoryResponse categoryResponse = categoryService.getAllCategories();
+        return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
     @PostMapping("/public/categories")
