8887759387f24f0f3edadac1d3d2feecbf7f26b2
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-03-31 14:00:37 +0700

    Logout Functionality

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -145,4 +145,11 @@ export const registerNewUser
         } finally {
             setLoader(false);
         }
-}
+};
+
+
+export const logOutUser = (navigate) => (dispatch) => {
+    dispatch({ type:"LOG_OUT" });
+    localStorage.removeItem("auth");
+    navigate("/login");
+};
