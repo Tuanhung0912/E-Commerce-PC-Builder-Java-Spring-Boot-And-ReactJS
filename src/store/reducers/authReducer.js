007b512b25fdf0689f822a86c29c1c43a6202f3b
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-03-31 16:31:40 +0700

    Allow Selected Address Function

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,6 +1,7 @@
 const initialState = {
     user: null,
     address: [],
+    selectedUserCheckoutAddress: null,
 }
 
 export const authReducer = (state = initialState, action) => {
@@ -16,6 +17,9 @@ export const authReducer = (state = initialState, action) => {
         
         case "USER_ADDRESS":
             return { ...state, address: action.payload };
+
+        case "SELECT_CHECKOUT_ADDRESS":
+            return { ...state, selectedUserCheckoutAddress: action.payload };
     
         default:
             return state;
