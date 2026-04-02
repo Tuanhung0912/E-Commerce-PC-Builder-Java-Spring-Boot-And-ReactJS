c327e6307fa71228fc3a99e06385a10d7077c3ed
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-04-02 16:57:58 +0700

    Image Update Form Layout

--- src/components/admin/products/AdminProducts.jsx
+++ src/components/admin/products/AdminProducts.jsx
@@ -11,6 +11,7 @@ import AddProductForm from './AddProductForm';
 import DeleteModal from '../../shared/DeleteModal';
 import { deleteProduct } from '../../../store/actions';
 import toast from 'react-hot-toast';
+import ImageUploadForm from './ImageUploadForm';
 
 const AdminProducts = () => {
   const {products, pagination} = useSelector((state) => state.products);
@@ -25,6 +26,7 @@ const AdminProducts = () => {
   const [openUpdateModal, setOpenUpdateModal] = useState(false);
   const [openAddModal, setOpenAddModal] = useState(false);
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const [openImageUploadModal, setOpenImageUploadModal] = useState(false);
 
   const [loader, setLoader] = useState(false);
 
@@ -54,7 +56,8 @@ const handleDelete = (product) => {
 };
 
 const handleImageUpload = (product) => {
-
+  setSelectedProduct(product);
+  setOpenImageUploadModal(true);
 };
 
 const handleProductView = (product) => {
@@ -143,6 +146,16 @@ const onDeleteHandler = () => {
           setOpen={openUpdateModal ? setOpenUpdateModal : setOpenAddModal}
           product={selectedProduct}
           update={openUpdateModal}
+          />
+    </Modal>
+
+    <Modal
+      open={openImageUploadModal}
+      setOpen={setOpenImageUploadModal}
+      title="Add Product Image">
+        <ImageUploadForm 
+          setOpen={setOpenImageUploadModal}
+          product={selectedProduct}
           />
     </Modal>
 
