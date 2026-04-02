4ecde8b714b3507696dd456e0d3495eb69e0c00e
tuanhung0912 <tuanhung.bh2003@gmail.com>
2026-04-02 13:12:26 +0700

    Private Route for Admin Only

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             <Route path='/register' element={ <Register />}/>
           </Route>
 
-          <Route path='/' element={<PrivateRoute />}>
+          <Route path='/' element={<PrivateRoute adminOnly />}>
             <Route path='/admin' element={ <AdminLayout />}/>
           </Route>
 
